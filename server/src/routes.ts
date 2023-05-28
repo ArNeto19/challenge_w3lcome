a539a1775aa90241bd05dd034d31631cb0b14181
arneto19 <armandopdneto@gmail.com>
2023-05-28 00:32:54 -0300

    feat: add controller methods on different requests
    Also return a html in case of client requesting to the home route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,13 +6,19 @@ const routes = express.Router();
 
 const taskController = new TaskController();
 
-routes.get("/", taskController.getTasks);
+routes.get("/", (req: express.Request, res: express.Response) => {
+  res
+    .status(200)
+    .send(
+      "<html>Please, access the tasks route to get proper response: <a href='/tasks'>/tasks</a></html>"
+    );
+});
 
 routes
   .route("/tasks")
   .get(taskController.getTasks)
   .post(taskController.create)
-  .patch(taskController.getTasks)
-  .delete(taskController.getTasks);
+  .patch(taskController.update)
+  .delete(taskController.delete);
 
 export default routes;
