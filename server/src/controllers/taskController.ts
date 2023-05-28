4960badeb6414d7a0c07fac56f6a4d355c5ea475
arneto19 <armandopdneto@gmail.com>
2023-05-28 00:30:33 -0300

    feat: add update and delete methods on controller
    refactor: minor adjustments on all methods

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -8,7 +8,7 @@ export class TaskController {
     const result: Task[] = tasks;
 
     if (!result) {
-      res.status(404).send({ message: "No tasks found." });
+      return res.status(404).send({ message: "No tasks found." });
     }
 
     res.status(200).send(result);
@@ -17,14 +17,20 @@ export class TaskController {
   create(req: express.Request, res: express.Response) {
     const { title } = req.body;
 
-    const result: Task[] = tasks;
-
     if (!title) {
-      return res.status(406).send({ error: "Give a title to your task." });
+      return res.status(400).send({ error: "Give a title to your task." });
     }
 
+    const result: Task[] = tasks;
+
+    if (!result) {
+      return res.status(404).send({ message: "No tasks found." });
+    }
+
+    const lastTaskId = result[result.length - 1].id;
+
     const newTask: Task = {
-      id: result.length + 1,
+      id: lastTaskId + 1,
       titulo: title,
       concluida: false,
     };
@@ -34,5 +40,54 @@ export class TaskController {
       message: "New task created.",
       newTask,
     });
+  }
+
+  update(req: express.Request, res: express.Response) {
+    const { id, title, check } = req.body;
+
+    const result: Task[] = tasks;
+    if (!result) {
+      return res.status(404).send({ message: "No tasks found." });
+    }
+
+    const currentTask: Task = result.find((e) => e.id === id);
+    if (!currentTask) {
+      return res.status(400).send({ message: "Task not found." });
+    }
+
+    const index = result.indexOf(currentTask);
+
+    if (title) {
+      result[index].titulo = title;
+    }
+
+    if (check) {
+      result[index].concluida = check;
+    }
+
+    res.status(204).send({
+      messsage: "Task updated.",
+      currentTask,
+    });
+  }
+
+  delete(req: express.Request, res: express.Response) {
+    const { id } = req.body;
+
+    const result: Task[] = tasks;
+    if (!result) {
+      return res.status(404).send({ message: "No tasks found." });
+    }
+
+    const currentTask: Task = result.find((e) => e.id === id);
+    if (!currentTask) {
+      return res.status(400).send({ message: "Task not found." });
+    }
+
+    const index = result.indexOf(currentTask);
+
+    result.splice(index, 1);
+
+    return res.status(200).send({ removedTask: currentTask });
   }
 }
